5ce04a6ef8d03301db1835ed373832cd5d84bd64
Preocts <preocts@preocts.com>
2023-04-09 01:26:46 -0400

    Update counts only on screen render

--- src/pdvconsole/vconsole.py
+++ src/pdvconsole/vconsole.py
@@ -127,7 +127,6 @@ class VConsole:
     def update(self, incident: Incident) -> None:
         """Update the VConsole."""
         self._incidents[incident.pdid] = incident
-        self._update_counts()
 
     def clean(self) -> None:
         """Remove incidents not updated after the POLL_TIME_SECONDS parameter."""
@@ -135,9 +134,8 @@ class VConsole:
         for incident in list(self._incidents.values()):
             if (now - incident.last_seen).seconds > self.update_interval:
                 del self._incidents[incident.pdid]
-        self._update_counts()
 
-    def _update_counts(self) -> None:
+    def update_counts(self) -> None:
         """Update the incident counts."""
         self.total_incidents = len(self.incidents)
         self.total_triggered = len(
@@ -252,6 +250,7 @@ def render_incident_panel(pd_details: VConsole, panel_height: int) -> Panel:
         hidden_count = 0
         incidents_ = pd_details.incidents
 
+    pd_details.update_counts()
     string_version = []
     for incident in incidents_:
         created_at = datetime.strptime(incident.created_at, "%Y-%m-%dT%H:%M:%SZ")
